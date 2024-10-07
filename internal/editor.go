10c96703efa344001c06e6ba5e033acaca876312
Quentin Quaadgras <email@quentinquaadgras.com>
2024-10-07 21:35:14 +1300

    add icons for editor.

--- internal/editor.go
+++ internal/editor.go
@@ -8,10 +8,18 @@ Editor is the main UI for editing a space in Aviary.
 type Editor struct {
 	gd.Class[Editor, gd.Control] `gd:"AviaryEditor"`
 
-	Foliage gd.TextureButton
-	Mineral gd.TextureButton
-	Shelter gd.TextureButton
-	Terrain gd.TextureButton
-	Citizen gd.TextureButton
-	Critter gd.TextureButton
+	Toolkit struct {
+		gd.PanelContainer
+
+		Buttons struct {
+			gd.VBoxContainer
+
+			Foliage gd.TextureButton
+			Mineral gd.TextureButton
+			Shelter gd.TextureButton
+			Terrain gd.TextureButton
+			Citizen gd.TextureButton
+			Critter gd.TextureButton
+		}
+	}
 }
