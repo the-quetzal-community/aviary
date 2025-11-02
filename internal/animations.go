e1750adff3a23fca2a9d1b86bc56251cfe722d6d
Quentin Quaadgras <email@quentinquaadgras.com>
2025-11-02 22:00:50 +1300

    make saving faster

--- internal/animations.go
+++ internal/animations.go
@@ -9,9 +9,12 @@ import (
 	"graphics.gd/classdb/SceneTree"
 	"graphics.gd/classdb/TextureRect"
 	"graphics.gd/classdb/Viewport"
+	"graphics.gd/variant/Object"
 	"graphics.gd/variant/Vector2"
 	"the.quetzal.community/aviary/internal/musical"
 )
+
+var currently_saving bool
 
 // AnimationSaving is played when the scene is explicitly saved with Ctrl+S
 type AnimationSaving struct {
@@ -19,14 +22,18 @@ type AnimationSaving struct {
 }
 
 // AnimateTheSceneBeingSaved animates the scene being saved by adding [AnimationSaving]
-// to the [SceneTree].
+// to the [SceneTree]. TODO/FIXME make GUI hidden.
 func AnimateTheSceneBeingSaved(parent Node.Any, name musical.Unique) {
-	tex := ImageTexture.CreateFromImage(Viewport.Get(parent.AsNode()).GetTexture().AsTexture2D().GetImage())
+	if currently_saving {
+		return
+	}
+	currently_saving = true
+	tex := Object.Leak(ImageTexture.CreateFromImage(Viewport.Get(parent.AsNode()).GetTexture().AsTexture2D().GetImage()))
 	saving := new(AnimationSaving)
 	saving.AsTextureRect().SetTexture(tex.AsTexture2D())
 	parent.AsNode().AddChild(saving.AsNode())
-
 	tex.AsTexture2D().GetImage().SavePng("user://" + base64.RawURLEncoding.EncodeToString(name[:]) + ".png")
+	currently_saving = false
 }
 
 // Ready implements [Node.Interface.Ready]
