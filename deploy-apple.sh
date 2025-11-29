61e43e971e8b907214e9f254c1c624b03a9de9be
Quentin Quaadgras <email@quentinquaadgras.com>
2025-11-29 20:58:55 +1300

    fix apple deployment script

--- deploy-apple.sh
+++ deploy-apple.sh
@@ -1,6 +1,12 @@
-GOOS=darwin gd build
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+gd build
 vpk download http --url "https://vpk.quetzal.community" -o ./releases/velopack
-vpk [osx] pack --packId "Aviary.EditorCollection" --packVersion "0.0.15" --packDir ./releases/darwin/universal/aviary.app -o ./releases/velopack \
+vpk pack --packId "Aviary.EditorCollection" --packVersion "$1" --mainExe aviary --packDir ./releases/darwin/universal/aviary.app -o ./releases/velopack \
     --signAppIdentity "Developer ID Application: Quentin Quaadgras" \
     --signInstallIdentity "Developer ID Installer: Quentin Quaadgras" \
     --notaryProfile "QuentinQuaadgras"
+cd releases/velopack
+rclone copy -v --max-depth 1 . r2:aviary/
